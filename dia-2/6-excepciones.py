4fcd968cfb4fb23548516ad5c023b63f92a12738
Guillermo Tapia <guillermo.tapia1991@gmail.com>
2023-05-17 18:46:21 -0500

    día2-Semana01

---
+++ dia-2/6-excepciones.py
@@ -0,0 +1,17 @@
+try:
+    print(5/'a')
+except ZeroDivisionError:
+    print('Error al hacer la división entre 0')
+
+except TypeError:
+    print('Error por cuestiones de tipos de datos')
+
+except Exception:
+    print('Ya no se cual es el error!')
+else:
+    #en el caso que nunca ingrese a ningun except
+    print('Todo Bien')
+finally:
+    #ingresa ya sea si estuvo bien o si entro a algun except
+    print('Yo ingreso SI O SI')
+print('Yo soy el final del archivo')
