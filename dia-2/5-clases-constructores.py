cb925455dbd39b447743c7fee2c991b2f7020eaa
Guillermo Tapia <guillermo.tapia1991@gmail.com>
2023-05-17 19:31:16 -0500

    add2 semana01

--- dia-2/5-clases-constructores.py
+++ dia-2/5-clases-constructores.py
@@ -1,7 +1,7 @@
 class Producto:
-    #constructor() { ... }
-    def __init__(self,nombre, precio, disponibilidad, imagen):
-        #al momento de crear una instancia se debera declarar los parametros
+    # constructor() { ... }
+    def __init__(self, nombre, precio, disponibilidad, imagen):
+        # al momento de crear una instancia se debera declarar los parametros
         self.nombre = nombre
         self.precio = precio
         self.disponibilidad = disponibilidad
@@ -10,6 +10,7 @@ class Producto:
     def mostrar_imagen(self):
         return self.imagen
 
-shampoo = Producto('Shampoo',7.40, True, 'https://google.com')
 
+shampoo = Producto('Shampoo', 7.40, True, 'https://google.com')
+shampoo.imagen = 'https://youtube.com'
 print(shampoo.mostrar_imagen())
--- dia-2/6-excepciones.py
+++ dia-2/6-excepciones.py
@@ -1,17 +1,18 @@
 try:
-    print(5/'a')
+    print(5/0)
 except ZeroDivisionError:
-    print('Error al hacer la división entre 0')
-
+    print('Error al hacer la division entre 0')
+    
 except TypeError:
     print('Error por cuestiones de tipos de datos')
 
 except Exception:
     print('Ya no se cual es el error!')
 else:
-    #en el caso que nunca ingrese a ningun except
-    print('Todo Bien')
+    # en el caso que nunca ingrese a ningun except
+    print('TODO BIEN!')
 finally:
-    #ingresa ya sea si estuvo bien o si entro a algun except
+    # ingresa ya sea si estuvo bien o si entro a algun except
     print('Yo ingreso SI O SI')
+
 print('Yo soy el final del archivo')
