0231ba223eaaf715eedcb01995bd581dd4eaff9d
Guillermo Tapia <guillermo.tapia1991@gmail.com>
2023-07-08 22:30:56 -0500

    imagenes con aws

---
+++ src/dtos/persona_dto.py
@@ -0,0 +1,10 @@
+from marshmallow_sqlalchemy import SQLAlchemyAutoSchema
+from marshmallow import Schema, fields
+from models.persona import Persona
+
+class PersonaResponseDto(SQLAlchemyAutoSchema):
+    class Meta:
+        model = Persona
+
+class PersonaRequestDto(SQLAlchemyAutoSchema):
+  nombre = fields.Str(required=True)
